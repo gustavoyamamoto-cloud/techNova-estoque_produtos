6240d860ed32fc75357661d57ea1be986128e8c0
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-02-05 18:33:35 -0300

    criação de setters e getters

--- src/Model/Produto.java
+++ src/Model/Produto.java
@@ -33,19 +33,37 @@ public class Produto {
 
 
     public void setId(int id) {
-        
+        if(getId() <= 0){
+            throw new IllegalArgumentException("Id inválido");
+        }
         this.id = id;
     }
+    
     public void setNome(String nome) {
+        if(getNome() == null || getNome().trim().isEmpty()){
+            throw new IllegalArgumentException("Nome inválido");
+        }
         this.nome = nome;
     }
+    
     public void setCategoria(Categoria categoria) {
+        if(getCategoria() == null){
+            throw new IllegalArgumentException("Categoria inválida");
+        }
         this.categoria = categoria;
     }
+    
     public void setPreco(double preco) {
+        if(getPreco() < 0){
+            throw new IllegalArgumentException("Preço inválido");
+        }
         this.preco = preco;
     }
+    
     public void setQuantEmEstoque(int quantEmEstoque) {
+        if(getQuantEmEstoque() < 0){
+            throw new IllegalArgumentException("Quantidade inválida");
+        }
         this.quantEmEstoque = quantEmEstoque;
     }
 
