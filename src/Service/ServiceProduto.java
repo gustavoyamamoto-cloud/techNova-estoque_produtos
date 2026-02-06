14a6b49462df0d6bcfe5e02eb64ced529f0b0794
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-02-05 21:56:29 -0300

    Terminado o serviceProduto

--- src/Service/ServiceProduto.java
+++ src/Service/ServiceProduto.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import Exceptions.EstoqueInsuficienteException;
 import Exceptions.ProdutoJaExisteException;
+import Exceptions.ProdutoNaoEncontradoException;
 import Model.Produto;
 
 public class ServiceProduto {
@@ -30,4 +32,66 @@ public class ServiceProduto {
         set.add(a.getNome());
         list.add(a);
     }
+
+    public void remover(int id){
+
+        if(!map.containsKey(id)){
+            throw new ProdutoNaoEncontradoException("Id não encontrado");
+        }
+
+        Produto a = map.get(id);
+
+        map.remove(a.getId());
+        set.remove(a.getNome());
+        list.remove(id);
+    }
+
+    public void listar(){
+
+        for(Produto a : list){
+            System.out.println(a);
+        }
+    } 
+
+    public void atualizarPreco(int id, double novoValor){
+
+        if(!map.containsKey(id)){
+            throw new ProdutoNaoEncontradoException("Id não encontrado");
+        }
+
+        Produto a = map.get(id);
+
+        a.setPreco(novoValor);
+    }
+
+    public void entradaEstq(int id, int valorEntrada){
+        if(!map.containsKey(id)){
+            throw new ProdutoNaoEncontradoException("Id não encontrado");
+        }
+
+        if(valorEntrada > 0){
+            throw new EstoqueInsuficienteException("Valor negativo");
+        }
+
+        Produto a = map.get(id);
+
+        a.setQuantEmEstoque(valorEntrada + a.getQuantEmEstoque());
+    } 
+
+    public void saidaEstq(int id, int valorSaida){
+
+        if(!map.containsKey(id)){
+            throw new ProdutoNaoEncontradoException("Id não encontrado");
+        }
+
+         Produto a = map.get(id);
+
+        if(valorSaida <= 0 && valorSaida < a.getQuantEmEstoque()){
+                throw new EstoqueInsuficienteException("Valor inválido");
+        }
+
+        a.setQuantEmEstoque(valorSaida - a.getQuantEmEstoque());
+
+    }
+
 }
